57d6330d9c1d97b3b3b057589b7231f77ce8655b
Lainey <dev.lainey@gmail.com>
2023-09-18 10:36:33 -0400

    add header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,16 @@ import './styles/Header.css'
 //----- EXPORTS -----//
 export default function Header() {
     return (
-        <nav className="nav-bar">
-            <div>
-                <Link to="/">About</Link>
-            </div>
-            <div>
-                <Link to="/portfolio">Projects</Link>
-            </div>
-            <div>
-                <Link to="/work">Work</Link>
-            </div>
-            <div>
-                <Link to="/contact">Contact</Link>
-            </div>
-        </nav>
+        <div className='header'>
+            <nav className="nav-bar">
+                <div>
+                    <Link to="/dev">Developer</Link>
+                </div>
+            </nav>
+            <header>
+                <h1>CSS Cheatsheet</h1>
+                <p>Explore and effortlessly copy CSS code snippets with this CSS Cheatsheet, your go-to resource for streamlining web design and styling.</p>
+            </header>
+        </div>
     );
 }
